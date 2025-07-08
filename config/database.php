9acd4383ffd2083c8607e7039092a3036ee4c0d6
KUNE23 <nuansajunti8@gmail.com>
2025-07-08 19:53:56 +0700

    add pgsql database & Change route

--- config/database.php
+++ config/database.php
@@ -84,12 +84,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
+            'url' => env('DB_URL', 'postgresql://postgres.yfdodwfdpqgjtlreltpn:catatku@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'catatku'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
